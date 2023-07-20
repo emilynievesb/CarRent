fff2477aa0f64a5d86d41a048dc3de604d072c2a
emilynievesb <nemily1710@gmail.com>
2023-07-20 11:19:52 -0500

    feat: :sparkles: Base de datos
    Se actualizó el script de la base de datos, con los cambios necesarios

--- src/utils/db.sql
+++ src/utils/db.sql
@@ -1,5 +1,4 @@
 
-DROP DATABASE CarRentDB;
 CREATE DATABASE CarRentDB;
 USE CarRentDB;
 
@@ -10,15 +9,36 @@ CREATE TABLE roles (
     CONSTRAINT pk_roles PRIMARY KEY (id_rol)
 );
 
+/*TABLA TIPO DOCUMENTO*/
+CREATE TABLE tipo_documento(
+    id_tipo_doc INT AUTO_INCREMENT NOT NULL,
+    nombre_tipo_documento VARCHAR(60) NOT NULL,
+    CONSTRAINT pk_tipo_documento PRIMARY KEY (id_tipo_doc)
+);
+
+CREATE TABLE nacionalidades(
+    id_nacionalidad INT AUTO_INCREMENT NOT NULL,
+    nombre_nacionalidad VARCHAR(30) NOT NULL,
+    CONSTRAINT pk_nacionalidades PRIMARY KEY (id_nacionalidad)
+);
+
 /*TABLA USERS*/
 CREATE TABLE users(
     id_user INT AUTO_INCREMENT NOT NULL,
+    doc_user INT NOT NULL,
+    id_tipo_doc INT NOT NULL,
     nombre_user VARCHAR(60) NOT NULL,
+    correo_user VARCHAR(60) NOT NULL,
+    direccion_user VARCHAR(60) NOT NULL,
+    telefono_user INT(10) NOT NULL,
+    id_nacionalidad INT NOT NULL,
     nickname_user VARCHAR(40) NOT NULL,
     contrasena_user VARCHAR(16) NOT NULL,
     id_rol INT NOT NULL,
     CONSTRAINT pk_users PRIMARY KEY (id_user),
-    CONSTRAINT fk_UsersRol FOREIGN KEY (id_rol) REFERENCES roles(id_rol)
+    CONSTRAINT fk_UsersRol FOREIGN KEY (id_rol) REFERENCES roles(id_rol),
+    CONSTRAINT fk_UsersTipoDocumento FOREIGN KEY (id_tipo_doc) REFERENCES tipo_documento(id_tipo_doc),
+    CONSTRAINT fk_UsersNacionalidad FOREIGN KEY (id_nacionalidad) REFERENCES nacionalidades(id_nacionalidad)
 );
 
 /*TABLA ESTADO VIGENCIA*/
@@ -62,6 +82,7 @@ CREATE TABLE tecnicomec(
 CREATE TABLE tipo_novedad(
     id_tipo_novedad INT AUTO_INCREMENT NOT NULL,
     nombre_tipo_novedad VARCHAR(80) NOT NULL,
+    precio_tipo_novedad INT NOT NULL,
     CONSTRAINT pk_tipo_novedad PRIMARY KEY (id_tipo_novedad)
 );
 
@@ -70,6 +91,14 @@ CREATE TABLE tipo_carro(
     id_tipo_carro INT AUTO_INCREMENT NOT NULL,
     nombre_tipo_carro VARCHAR(60) NOT NULL,
     CONSTRAINT pk_tipo_carro PRIMARY KEY (id_tipo_carro)
+);
+
+CREATE TABLE sedes(
+    id_sede INT AUTO_INCREMENT NOT NULL,
+    ciudad_sede VARCHAR(30) NOT NULL,
+    telefono_sede INT(12) NOT NULL,
+    direccion_sede VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_sedes PRIMARY KEY (id_sede)
 );
 
 /*TABLA CARROS*/
@@ -85,10 +114,12 @@ CREATE TABLE carros(
     id_seguro INT NOT NULL,
     id_tecnicomec INT NOT NULL,
     id_tipo_carro INT NOT NULL,
+    id_sede INT NOT NULL,
     CONSTRAINT pk_carros PRIMARY KEY (id_carro),
     CONSTRAINT fk_CarrosSoat FOREIGN KEY (id_soat) REFERENCES soat(id_soat),
     CONSTRAINT fk_CarrosSeguro FOREIGN KEY (id_seguro) REFERENCES seguro(id_seguro),
     CONSTRAINT fk_CarrosTecnicomec FOREIGN KEY (id_tecnicomec) REFERENCES tecnicomec(id_tecnicomec),
+    CONSTRAINT fk_CarrosSede FOREIGN KEY (id_sede) REFERENCES sedes(id_sede),
     CONSTRAINT fk_CarrosTipoCarro FOREIGN KEY (id_tipo_carro) REFERENCES tipo_carro(id_tipo_carro)
 );
 
@@ -139,70 +170,135 @@ CREATE TABLE factura(
 
 /***********************INSERTS*/
 
-/* TABLA ROLES */
+
+/* INSERT TIPOS DE DOCUMENTO */
+INSERT INTO tipo_documento (nombre_tipo_documento) VALUES
+    ('Cédula de ciudadanía'),
+    ('Pasaporte'),
+    ('Cédula de extranjería');
+
+/* INSERT NACIONALIDADES */
+INSERT INTO nacionalidades (nombre_nacionalidad) VALUES
+    ('Colombia'),
+    ('Estados Unidos'),
+    ('España');
+
+/* INSERT ROLES */
 INSERT INTO roles (nombre_rol) VALUES
-    ('Administrador'),
-    ('Usuario');
+    ('Cliente'),
+    ('Administrador');
 
-/* TABLA USERS */
-INSERT INTO users (nombre_user, nickname_user, contrasena_user, id_rol) VALUES
-    ('Juan Pérez', 'juan123', 'contraseña123', 1),
-    ('María López', 'marialo', 'pass456', 2);
+/* INSERT SEDES */
+INSERT INTO sedes (ciudad_sede, telefono_sede, direccion_sede) VALUES
+    ('Bogotá', 1234567890, 'Calle 123 #45-67'),
+    ('Medellín', 9876543210, 'Carrera 678 #12-34'),
+    ('Cali', 5678901234, 'Avenida 987 #56-78');
 
-/* TABLA ESTADO VIGENCIA */
+/* INSERT ESTADO VIGENCIA */
 INSERT INTO estado_vigencia (nombre_estado) VALUES
     ('Vigente'),
     ('Vencido');
 
-/* TABLA SOAT */
+/* INSERT SOAT */
 INSERT INTO soat (fecha_inicio_soat, fecha_vencimiento_soat, id_estado) VALUES
     ('2023-01-01', '2024-01-01', 1),
-    ('2022-06-15', '2023-06-15', 2);
+    ('2023-02-15', '2024-02-15', 1),
+    ('2023-03-20', '2024-03-20', 1),
+    ('2023-04-10', '2024-04-10', 1),
+    ('2023-05-05', '2024-05-05', 1),
+    ('2023-06-12', '2024-06-12', 1);
 
-/* TABLA SEGURO */
+/* INSERT SEGURO */
 INSERT INTO seguro (fecha_inicio_seguro, fecha_vencimiento_seguro, id_estado) VALUES
     ('2023-01-01', '2024-01-01', 1),
-    ('2022-06-15', '2023-06-15', 2);
+    ('2023-02-15', '2024-02-15', 1),
+    ('2023-03-20', '2024-03-20', 1),
+    ('2023-04-10', '2024-04-10', 1),
+    ('2023-05-05', '2024-05-05', 1),
+    ('2023-06-12', '2024-06-12', 1);
 
-/* TABLA TECNICOMEC */
+/* INSERT TECNICOMEC */
 INSERT INTO tecnicomec (fecha_inicio_tecnicomec, fecha_vencimiento_tecnicomec, id_estado) VALUES
     ('2023-01-01', '2024-01-01', 1),
-    ('2022-06-15', '2023-06-15', 2);
+    ('2023-02-15', '2024-02-15', 1),
+    ('2023-03-20', '2024-03-20', 1),
+    ('2023-04-10', '2024-04-10', 1),
+    ('2023-05-05', '2024-05-05', 1),
+    ('2023-06-12', '2024-06-12', 1);
 
-/* TABLA TIPO DE NOVEDAD */
-INSERT INTO tipo_novedad (nombre_tipo_novedad) VALUES
-    ('Daño exterior'),
-    ('Daño interior'),
-    ('Problema mecánico');
+/* INSERT TIPO DE NOVEDAD */
+INSERT INTO tipo_novedad (nombre_tipo_novedad, precio_tipo_novedad) VALUES
+    ('Rasguño pequeño', 50000),
+    ('Abolladura', 80000),
+    ('Rotura de espejo', 100000),
+    ('Daño en llanta', 60000),
+    ('Rasguño grande', 70000),
+    ('Rotura de vidrio', 120000);
 
-/* TABLA TIPO DE CARRO */
+/* INSERT HISTORIAL NOVEDADES */
+INSERT INTO historial_novedades (acumulado_daños) VALUES
+    (0),
+    (150000),
+    (300000),
+    (500000),
+    (700000),
+    (850000);
+
+/* INSERT TIPO DE CARRO */
 INSERT INTO tipo_carro (nombre_tipo_carro) VALUES
     ('Sedán'),
     ('SUV'),
-    ('Camioneta');
+    ('Camioneta'),
+    ('Deportivo'),
+    ('Compacto'),
+    ('Furgoneta');
 
-/* TABLA CARROS */
-INSERT INTO carros (marca_carro, modelo_carro, numero_puertas_carro, color_carro, precio_hora_carro, placa_carro, id_soat, id_seguro, id_tecnicomec, id_tipo_carro) VALUES
-    ('Toyota', 'Corolla', 4, 'Rojo', 100, 'ABC123', 1, 1, 1, 1),
-    ('Honda', 'CR-V', 4, 'Azul', 120, 'DEF456', 2, 2, 2, 2);
 
-/* TABLA HISTORIAL NOVEDADES */
-INSERT INTO historial_novedades (acumulado_daños) VALUES
-    (500),
-    (1000);
+/* INSERT CARROS */
+INSERT INTO carros (marca_carro, modelo_carro, numero_puertas_carro, color_carro, precio_hora_carro, placa_carro, id_soat, id_seguro, id_tecnicomec, id_tipo_carro, id_sede) VALUES
+    ('Toyota', 'Corolla', 4, 'Negro', 50000, 'ABC123', 1, 2, 3, 1, 1),
+    ('Honda', 'Civic', 4, 'Gris', 45000, 'DEF456', 2, 3, 4, 1, 2),
+    ('Chevrolet', 'Spark', 5, 'Blanco', 40000, 'GHI789', 3, 4, 5, 2, 1),
+    ('Ford', 'Mustang', 2, 'Rojo', 70000, 'JKL012', 4, 5, 6, 2, 2),
+    ('Mazda', 'CX-5', 4, 'Azul', 55000, 'MNO345', 5, 6, 1, 1, 3),
+    ('Nissan', 'Sentra', 4, 'Plateado', 48000, 'PQR678', 6, 1, 2, 1, 3);
 
-/* TABLA NOVEDADES */
+/* INSERT NOVEDADES */
 INSERT INTO novedades (id_tipo_novedad, descripcion_novedad, id_historial) VALUES
-    (1, 'Rasguño en la puerta izquierda', 1),
-    (2, 'Tapicería manchada', 1),
-    (3, 'Problema en la transmisión', 2);
+    (1, 'Pequeño rasguño en la puerta izquierda.', 2),
+    (2, 'Abolladura en la parte trasera del vehículo.', 3),
+    (3, 'Espejo lateral izquierdo roto.', 1),
+    (4, 'Daño en la llanta delantera derecha.', 4),
+    (5, 'Rasguño grande en el capó.', 5),
+    (6, 'Vidrio trasero roto.', 6);
 
-/* TABLA REPORT ALQUILER */
+/* INSERT USERS */
+INSERT INTO users (doc_user, id_tipo_doc, nombre_user, correo_user, direccion_user, telefono_user, id_nacionalidad, nickname_user, contrasena_user, id_rol) VALUES
+    (12345678, 1, 'Juan Pérez', 'juan@example.com', 'Calle 123 #45-67', 3001234567, 1, 'juan123', 'contrasena123', 1),
+    (98765432, 2, 'Jane Smith', 'jane@example.com', 'Avenue 456 #78-90', 4009876543, 2, 'jane456', 'password456', 1),
+    (87654321, 1, 'Carlos González', 'carlos@example.com', 'Carrera 789 #12-34', 2008765432, 1, 'carlos77', 'pass77', 1),
+    (54321678, 1, 'Emily Brown', 'emily@example.com', 'Street 345 #67-89', 5005432167, 2, 'emilyB', 'p@ssw0rd', 1),
+    (65437890, 3, 'Michael Johnson', 'michael@example.com', 'Main Road 890 #45-67', 7006543789, 2, 'mikeJ', 'qwerty123', 1),
+    (78901234, 1, 'Laura Fernández', 'laura@example.com', 'Plaza 567 #12-34', 1007890123, 1, 'laurita', 'abcde567', 1);
+
+/* INSERT ADMINISTRADOR */
+INSERT INTO users (doc_user, id_tipo_doc, nombre_user, correo_user, direccion_user, telefono_user, id_nacionalidad, nickname_user, contrasena_user, id_rol) VALUES
+    (12340001, 1, 'Admin CarRent', 'admin@example.com', 'Admin Avenue 1', 9999999999, 1, 'admin', 'admin123', 2);
+
+/* INSERT REPORT ALQUILER */
 INSERT INTO reporte_alquiler (id_user, id_carro, fecha_inicio_alquiler, fecha_final_alquiler, precio_cotizado_alquiler, monto_fianza, id_historial_novedades) VALUES
-    (1, 1, '2023-07-01', '2023-07-05', 400, 200, 1),
-    (2, 2, '2023-07-03', '2023-07-07', 480, 250, 2);
+    (1, 1, '2023-07-01', '2023-07-08', 350000, 200000, 1),
+    (2, 2, '2023-07-05', '2023-07-09', 320000, 180000, 2),
+    (3, 3, '2023-07-10', '2023-07-13', 240000, 150000, 3),
+    (4, 4, '2023-07-15', '2023-07-21', 420000, 250000, 4),
+    (5, 5, '2023-07-18', '2023-07-20', 440000, 270000, 5),
+    (6, 6, '2023-07-20', '2023-07-23', 270000, 150000, 6);
 
-/* TABLA FACTURA */
+/* INSERT FACTURA */
 INSERT INTO factura (id_reporte_alquiler, fecha_final_real, dias_extra, total_pago_alquiler) VALUES
-    (1, '2023-07-05 12:00:00', 0, 400),
-    (2, '2023-07-08 10:00:00', 1, 530);
+    (1, '2023-07-08 12:00:00', 0, 350000),
+    (2, '2023-07-09 10:30:00', 1, 330000),
+    (3, '2023-07-13 09:15:00', 0, 240000),
+    (4, '2023-07-21 14:45:00', 0, 420000),
+    (5, '2023-07-20 17:30:00', 0, 440000),
+    (6, '2023-07-23 11:00:00', 0, 270000);
